a0a175cb6d9745cb7c02e278f3547a9885c29394
alejorostata <arostata@gbox.adnu.edu.ph>
2026-08-16 00:08:59 +0800

    Fix CLS on desktop by removing LazySection and relying on server rendering

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,13 @@ import { LanguageProvider } from '@/context/LanguageContext';
 import { Header } from '@/components/Header';
 import { HeroSection } from '@/components/HeroSection';
 import { Footer } from '@/components/Footer';
-import { LazySection } from '@/components/LazySection';
 import dynamic from 'next/dynamic';
 
-const DynamicExperienceSection = dynamic(() => import('@/components/ExperienceSection').then(m => m.ExperienceSection), { ssr: false });
-const DynamicSkillsSection = dynamic(() => import('@/components/SkillsSection').then(m => m.SkillsSection), { ssr: false });
-const DynamicProjectsSection = dynamic(() => import('@/components/ProjectsSection').then(m => m.ProjectsSection), { ssr: false });
-const DynamicEducationLeadershipSection = dynamic(() => import('@/components/EducationLeadershipSection').then(m => m.EducationLeadershipSection), { ssr: false });
-const DynamicContactSection = dynamic(() => import('@/components/ContactSection').then(m => m.ContactSection), { ssr: false });
+const DynamicExperienceSection = dynamic(() => import('@/components/ExperienceSection').then(m => m.ExperienceSection));
+const DynamicSkillsSection = dynamic(() => import('@/components/SkillsSection').then(m => m.SkillsSection));
+const DynamicProjectsSection = dynamic(() => import('@/components/ProjectsSection').then(m => m.ProjectsSection));
+const DynamicEducationLeadershipSection = dynamic(() => import('@/components/EducationLeadershipSection').then(m => m.EducationLeadershipSection));
+const DynamicContactSection = dynamic(() => import('@/components/ContactSection').then(m => m.ContactSection));
 const DynamicResumeModal = dynamic(() => import('@/components/ResumeModal').then(m => m.ResumeModal), { ssr: false });
 
 export default function Home() {
@@ -48,21 +47,11 @@ export default function Home() {
           {/* Main Content Area */}
           <main id="main-content" className="flex-grow" tabIndex={-1}>
             <HeroSection />
-            <LazySection minHeight="800px">
-              <DynamicExperienceSection />
-            </LazySection>
-            <LazySection minHeight="800px">
-              <DynamicSkillsSection />
-            </LazySection>
-            <LazySection minHeight="1200px">
-              <DynamicProjectsSection />
-            </LazySection>
-            <LazySection minHeight="600px">
-              <DynamicEducationLeadershipSection />
-            </LazySection>
-            <LazySection minHeight="600px">
-              <DynamicContactSection />
-            </LazySection>
+            <DynamicExperienceSection />
+            <DynamicSkillsSection />
+            <DynamicProjectsSection />
+            <DynamicEducationLeadershipSection />
+            <DynamicContactSection />
           </main>
 
           {/* Footer */}
